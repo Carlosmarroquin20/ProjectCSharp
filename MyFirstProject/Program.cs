252e88f4641cf666d41b53cddf760e9af3492e39
Emanuel Marroquin <carlosema322@gmail.com>
2025-01-28 18:30:29 -0600

    Add project files.

---
+++ MyFirstProject/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace HelloWorld
+{
+    internal class DataTypes
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = 20;
+            int z = 30;
+
+
+            int age = 21;
+            Console.WriteLine(age);
+
+            long bigNumber = 488888l;
+            Console.WriteLine(bigNumber);
+
+            double negative = -3.4D;
+            Console.WriteLine(negative);
+
+            float precision = 5.000001F;
+            Console.WriteLine(precision);
+
+            decimal money = 14.99M;
+            Console.WriteLine(money);
+
+            Console.ReadLine();
+        }
+    }
+}
