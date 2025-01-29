a5c6e77e8d20de38822cf7225cdaf9131cb7ed72
Emanuel Marroquin <carlosema322@gmail.com>
2025-01-29 13:09:02 -0600

    Add string to numeric conversions using Convert class in C#

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -4,18 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace String_Chards
+namespace HelloWorld
 {
     internal class DataTypes
     {
         static void Main(string[] args)
         {
-            string name = "Ema";
-            char letter = 'A';
+            string textAge = "-22";
+            int age = Convert.ToInt32(textAge); 
+            Console.WriteLine(age);
 
-            Console.WriteLine(name);
-            Console.WriteLine(letter);
+            string textBigNumber = "488888";
+            long bigNumber = Convert.ToInt64(textBigNumber);
+            Console.WriteLine(bigNumber);
 
+            string textNegative = "-3.4";
+            double negative = Convert.ToDouble(textNegative);
+            Console.WriteLine(negative);
+
+            string textPrecision = "5.000001";
+            float precision = Convert.ToSingle(textPrecision);
+            Console.WriteLine(precision);
+
+            string textMoney = "14.99";
+            decimal money = Convert.ToDecimal(textMoney);
+            Console.WriteLine(money);
 
             Console.ReadLine();
         }
