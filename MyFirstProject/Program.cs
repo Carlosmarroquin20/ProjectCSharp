290b71196ca40c213577c01952e4372555dd8c4d
Emanuel Marroquin <carlosema322@gmail.com>
2025-01-29 11:35:12 -0600

    Add examples of string and char data types with console output

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -4,31 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace HelloWorld
+namespace String_Chards
 {
     internal class DataTypes
     {
         static void Main(string[] args)
         {
-            int x = 10;
-            int y = 20;
-            int z = 30;
+            string name = "Ema";
+            char letter = 'A';
 
+            Console.WriteLine(name);
+            Console.WriteLine(letter);
 
-            int age = 22;
-            Console.WriteLine(age);
-
-            long bigNumber = 488888l;
-            Console.WriteLine(bigNumber);
-
-            double negative = -3.4D;
-            Console.WriteLine(negative);
-
-            float precision = 5.000001F;
-            Console.WriteLine(precision);
-
-            decimal money = 14.99M;
-            Console.WriteLine(money);
 
             Console.ReadLine();
         }
