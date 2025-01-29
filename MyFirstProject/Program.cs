951cf326ba7e8555c7ec608ad045768997e73a0c
Emanuel Marroquin <carlosema322@gmail.com>
2025-01-29 13:15:09 -0600

    Add examples of bool type and updating its value in C#

--- MyFirstProject/Program.cs
+++ MyFirstProject/Program.cs
@@ -10,25 +10,13 @@ namespace HelloWorld
     {
         static void Main(string[] args)
         {
-            string textAge = "-22";
-            int age = Convert.ToInt32(textAge); 
-            Console.WriteLine(age);
+            bool value = true;
 
-            string textBigNumber = "488888";
-            long bigNumber = Convert.ToInt64(textBigNumber);
-            Console.WriteLine(bigNumber);
+            bool isMale = true;
+            Console.WriteLine(isMale);
 
-            string textNegative = "-3.4";
-            double negative = Convert.ToDouble(textNegative);
-            Console.WriteLine(negative);
-
-            string textPrecision = "5.000001";
-            float precision = Convert.ToSingle(textPrecision);
-            Console.WriteLine(precision);
-
-            string textMoney = "14.99";
-            decimal money = Convert.ToDecimal(textMoney);
-            Console.WriteLine(money);
+            isMale = false;
+            Console.WriteLine(isMale);
 
             Console.ReadLine();
         }
