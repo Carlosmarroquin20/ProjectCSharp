a327c2ec0d7485b8b7da2673c6d45813e723e19d
Emanuel Marroquin <carlosema322@gmail.com>
2025-01-30 15:12:10 -0600

    Add examples of modulus operator for calculating remainders in C#

--- MyFirstProject/First/Program.cs
+++ MyFirstProject/First/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Execercise2;
 using Exercise1;
 
 namespace MyFirstProject
@@ -11,7 +12,7 @@ namespace MyFirstProject
     {
         static void Main(string[] args)
         {
-            StoringUserData.Run(); 
+            Odd_EvenChecker.Run(); 
 
             Console.ReadLine();
 
---
+++ MyFirstProject/Odd_EvenChecker.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Execercise2
+{
+    internal class Odd_EvenChecker
+    {
+        public static void Run()
+        {
+            int num1 = 10;
+            int num2 = 2;
+            int remainder = num1 % num2;
+            Console.WriteLine(remainder);
+
+            num1 = 11;
+            remainder = num1 % num2;
+            Console.WriteLine(remainder);
+
+
+            Console.ReadLine();
+        }
+    }
+}
